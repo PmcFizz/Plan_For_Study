e19eda857f1d8968094945a4f8e3bfaa56ca5b79
MR●Pang <13631686641@sina.cn>
2016-08-22 16:30:44 +0800

    修改html为jsp 并在jsp页头加java代码
    修改html为jsp 并在jsp页头加java代码

--- Module/gulpfile.js
+++ Module/gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require("gulp"),
 	uglify = require("gulp-uglify"),
 	rename = require("gulp-rename"),
 	changed = require("gulp-changed"),
-	watch = require("gulp-watch");
+	watch = require("gulp-watch"),
+	header=require('gulp-header');
 
 var DEST = "build/js";
 var scriptsGlob = "js/*.js";
@@ -34,6 +35,23 @@ gulp.task("onlychange", function() {
 		.pipe(changed(DEST))
 		.pipe(uglify())
 		.pipe(gulp.dest(DEST));
+})
+
+
+//修改html为jsp 并在jsp页头加java代码
+var jspHeader=[];
+jspHeader.push('<!--请在这里写页面注释-->\n');
+jspHeader.push('<%= start %>@ page language="java" import="java.util.*" pageEncoding="UTF-8"<%= end %>\n');
+jspHeader.push('<%= start %>\nString path = request.getContextPath();\n');
+jspHeader.push('String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";\n<%= end %>\n');
+jspHeader=jspHeader.join('');
+gulp.task('htmltojsp', function() {
+		gulp.src("view/**/*.html")
+			.pipe(header(jspHeader,{start:"<%",end:"%>"}))
+			.pipe(rename({
+				extname: ".jsp"
+			}))
+			.pipe(gulp.dest("build"))
 })
 
 gulp.task("default", function() {
